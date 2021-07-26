b8eea0a5b514824f1cb74b5a802af1f11b890063
Jahaziel Rodriguez <68198368+Young-Prota@users.noreply.github.com>
2021-07-25 21:46:09 -0400

    Header Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     extend: {
       fontFamily: {
         ptserif: ['PT Serif']
+      },
+      animation: {
+        'spin-slow': 'spin 9s linear infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-9deg)' },
+          '50%': { transform: 'rotate(9deg)' },
+        }
       }
     },
   },
