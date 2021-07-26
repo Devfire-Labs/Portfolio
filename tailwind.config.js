4f6cda24ebf6a065e32d76d6884da77103745c15
Raul Luis <soyrauluis@outlook.com>
2021-07-26 15:34:39 -0400

    Configure TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        ptserif: ['PT Serif']
-      },
-      animation: {
-        'spin-slow': 'spin 9s linear infinite',
-      },
-      keyframes: {
-        wiggle: {
-          '0%, 100%': { transform: 'rotate(-9deg)' },
-          '50%': { transform: 'rotate(9deg)' },
-        }
-      },
-      colors: {
-        ProtaBlack: '#333333'
-      }
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: ['./*.html', './**/*.html'],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				ptserif: ['PT Serif'],
+			},
+			animation: {
+				'spin-slow': 'spin 9s linear infinite',
+			},
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(-9deg)' },
+					'50%': { transform: 'rotate(9deg)' },
+				},
+			},
+			colors: {
+				ProtaBlack: '#333333',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
