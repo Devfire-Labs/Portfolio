86342df99ba5a96a309c85a51ded6feaf7234479
Jahaziel Rodriguez <68198368+Young-Prota@users.noreply.github.com>
2021-08-11 23:41:15 -0400

    Started from scratch

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Menu from "./components/Menu.svelte";
+  export let name;
+</script>
+
+<main>
+  <Menu />
+  <Header />
+</main>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,12 @@
 module.exports = {
-	mode: 'jit',
-	purge: ['./*.html', './**/*.html'],
-	darkMode: 'media', // or 'media' or 'class'
-	theme: {
-		extend: {
-			fontFamily: {
-				ptserif: ['PT Serif'],
-			},
-			animation: {
-				'spin-slow': 'spin 9s linear infinite',
-			},
-			keyframes: {
-				wiggle: {
-					'0%, 100%': { transform: 'rotate(-9deg)' },
-					'50%': { transform: 'rotate(9deg)' },
-				},
-			},
-			colors: {
-				ProtaBlack: '#333333',
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-};
+  mode: 'jit',
+  purge: ['./public/index.html', './src/**/*.svelte'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
