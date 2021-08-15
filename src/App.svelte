5e3cd998c9cf08d384c3f022a5a7fc7ec568145c
Jahaziel Rodriguez <68198368+Young-Prota@users.noreply.github.com>
2021-08-15 15:39:37 -0400

    Created All Pages

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,9 @@
   import Menu from "./components/Menu.svelte";
   import Home from "./pages/Home.svelte";
   import Footer from "./components/Footer.svelte";
+  import Work from "./pages/Work.svelte";
+  import About from "./pages/About.svelte";
+  import Contact from "./pages/Contact.svelte";
 </script>
 
 <Menu />
---
+++ src/pages/Work.svelte
@@ -0,0 +1,68 @@
+<script>
+  import { menu } from "../stores/menu";
+</script>
+
+<!-- Title and filters -->
+<header>
+  <div class=" flex flex-col  ml-8 my-20 font-serif">
+    <h1 class=" text-5xl font-semibold text-red-400">Work</h1>
+    <div class=" flex flex-row text-2xl mt-4">
+      <h2 class=" text-red-400">All</h2>
+      <h2 class=" ml-4">UI/UX</h2>
+      <h2 class=" ml-4">Branding</h2>
+    </div>
+  </div>
+</header>
+
+<!-- First Object (Pocky Pop) -->
+<div>
+  <div class=" flex flex-col text-center font-serif text-lg">
+    <img class="object-cover h-96 m-8" src="/assets/img1.png" alt="Pocky Pop" />
+    <h2 class="font-bold ">Pocky Pop</h2>
+    <h2>Branding</h2>
+  </div>
+
+  <!-- Second Object (House App) -->
+  <div class=" flex flex-col text-center font-serif text-lg mt-8">
+    <img
+      class="object-cover h-96 m-8"
+      src="/assets/img2.png"
+      alt="House Marketplace"
+    />
+    <h2 class="font-bold ">House Marketplace App</h2>
+    <h2>UI/UX Design</h2>
+  </div>
+
+  <!-- Third Object (Pouf Co) -->
+  <div class=" flex flex-col text-center font-serif text-lg mt-8">
+    <img
+      class="object-cover h-96 m-8"
+      src="/assets/img5.png"
+      alt="Pouf Co Website"
+    />
+    <h2 class="font-bold ">Pouf Co Website</h2>
+    <h2>UI Design, Web Design</h2>
+  </div>
+
+  <!-- Fourth Object (Silvestre) -->
+  <div class=" flex flex-col text-center font-serif text-lg mt-8">
+    <img
+      class="object-cover h-96 m-8"
+      src="/assets/img6.png"
+      alt="Pouf Co Website"
+    />
+    <h2 class="font-bold ">Meat Packing by Silvestre</h2>
+    <h2>Branding</h2>
+  </div>
+
+  <!-- Fifth Object (Wendy) -->
+  <div class=" flex flex-col text-center font-serif text-lg mt-8 mb-12">
+    <img
+      class="object-cover h-96 m-8"
+      src="/assets/img7.png"
+      alt="Pouf Co Website"
+    />
+    <h2 class="font-bold ">Doña Wendy Bocadillos</h2>
+    <h2>Branding</h2>
+  </div>
+</div>
