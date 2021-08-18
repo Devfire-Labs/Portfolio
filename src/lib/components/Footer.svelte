ba85471e3dda606ff9794e5eff6544f7b5419b08
Raul Luis <soyrauluis@outlook.com>
2021-08-18 19:49:22 -0400

    Migrate from Svelte to SvelteKit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { menu } from '$lib/stores/menu';
+</script>
+
+<footer
+	class="border-t-2 border-gray-500 m-8 lg:m-16 py-8 lg:py-12 bottom-0 p-1 flex flex-wrap lg:flex-row justify-between md:items-center"
+>
+	<img
+		class=" object-contain h-16 md:h-20"
+		src="/assets/logofooter.svg"
+		alt="Logo"
+	/>
+
+	<img
+		class=" object-contain h-14 md:h-16 animate-spin-slow"
+		src="/assets/stamp2.svg"
+		alt="Stamp"
+	/>
+
+	<div class=" flex flex-row font-serif text-lg mt-8 md:mt-0">
+		<div>
+			<h2>Work</h2>
+			<h2>Branding</h2>
+			<h2>UI/UX</h2>
+			<h2>About Me</h2>
+		</div>
+		<div class=" ml-16">
+			<h2>Behance</h2>
+			<h2>LinkedIn</h2>
+			<h2>Polywork</h2>
+			<h2 class=" text-red-400">Contact Me</h2>
+		</div>
+	</div>
+</footer>
